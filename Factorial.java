430dd3ac74886747c03139afb4df3d1b0dbea4ce
Shravani2708 <shravanisetty27@gmail.com>
2019-01-07 14:55:17 +0530

    Updated three codes

--- Factorial.java
+++ Factorial.java
@@ -3,14 +3,15 @@ package MyPack;
 public class Factorial {
 
 	public static void main(String[] args) {
-		int i,fact=1;
-		int num=4;
+		int j,fact=1;
+		int num=5;
 		System.out.println("factorial of"+num+"is started");
-		for(i=1;i<=num;i++)
+		for(j=1;j<=num;j++)
 		{
 			fact=fact*i;
 		}
 		System.out.println("factorial of"+num+"is"+fact);
+		System.out.println("The given number is factorial");
 
 	}
 
